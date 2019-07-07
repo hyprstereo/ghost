44487770d63be6aaca167955e4714ea5840256e7
Gabriel Lim <hello@gabriellim.com>
2019-07-08 07:19:32 +0800

    searchable

--- resources/assets/js/components/views/investigations/InvestigationList.vue
+++ resources/assets/js/components/views/investigations/InvestigationList.vue
@@ -12,6 +12,38 @@
     </div>
     <a-divider/>
     <a-table :columns="columns" :dataSource="cases" @change="handleChange" >
+      <div slot="filterDropdown" slot-scope="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }" class='custom-filter-dropdown'>
+      <a-input
+        v-ant-ref="c => searchInput = c"
+        :placeholder="`Search ${column.dataIndex}`"
+        :value="selectedKeys[0]"
+        @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+        @pressEnter="() => handleSearch(selectedKeys, confirm)"
+        style="width: 188px; margin-bottom: 8px; display: block;"
+      />
+      <a-button
+        type='primary'
+        @click="() => handleSearch(selectedKeys, confirm)"
+        icon="search"
+        size="small"
+        style="width: 90px; margin-right: 8px"
+      >Search</a-button>
+      <a-button
+        @click="() => handleReset(clearFilters)"
+        size="small"
+        style="width: 90px"
+      >Reset</a-button>
+    </div>
+    <a-icon slot="filterIcon" slot-scope="filtered" type='search' :style="{ color: filtered ? '#108ee9' : undefined }" />
+    <template slot="customRender" slot-scope="text">
+      <span v-if="searchText">
+        <template v-for="(fragment, i) in text.toString().split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))">
+          <mark v-if="fragment.toLowerCase() === searchText.toLowerCase()" :key="i" class="highlight">{{fragment}}</mark>
+          <template v-else>{{fragment}}</template>
+        </template>
+      </span>
+      <template v-else>{{text}}</template>
+    </template>
       <template slot="operation" slot-scope="text, record">
         <a :href="getLink(record.key)">Report</a>
         <a-divider type="vertical" />
@@ -36,6 +68,8 @@ export default {
   data() {
     return {
       cases: [],
+      searchText: '',
+      searchInput: null,
     }
   },
   computed: {
@@ -47,29 +81,79 @@ export default {
         title: 'No. Kes',
         dataIndex: 'serial',
         key: 'serial',
-      }, {
+        scopedSlots: {
+          filterDropdown: 'filterDropdown',
+          filterIcon: 'filterIcon',
+          customRender: 'customRender'
+        },
+        onFilter: (value, record) => record.serial.toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible) => {
+          if (visible) {
+            setTimeout(() => {
+              this.searchInput.focus()
+            },0)
+          }
+        }
+      },
+      {
         title: 'Orang Kena Tuduh',
         dataIndex: 'accused',
         key: 'accused',
+        scopedSlots: {
+          filterDropdown: 'filterDropdown',
+          filterIcon: 'filterIcon',
+          customRender: 'customRender'
+        },
+        onFilter: (value, record) => record.accused.toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible) => {
+          if (visible) {
+            setTimeout(() => {
+              this.searchInput.focus()
+            },0)
+          }
+        }
       }, {
         title: 'Status',
         dataIndex: 'status',
         key: 'status',
-        filters: [
-          { text: 'New', value: 'New' },
-          { text: 'Open', value: 'Open' },
-          { text: 'Closed', value: 'Closed' },
-        ]
+        scopedSlots: {
+          filterDropdown: 'filterDropdown',
+          filterIcon: 'filterIcon',
+          customRender: 'customRender'
+        },
+        onFilter: (value, record) => record.status.toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible) => {
+          if (visible) {
+            setTimeout(() => {
+              this.searchInput.focus()
+            },0)
+          }
+        }
       },
       {
           title: 'Tarikh',
           dataIndex: 'date',
           key: 'date',
+          scopedSlots: {
+          filterDropdown: 'filterDropdown',
+          filterIcon: 'filterIcon',
+          customRender: 'customRender'
+        },
+        onFilter: (value, record) => record.date.toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownVisibleChange: (visible) => {
+          if (visible) {
+            setTimeout(() => {
+              this.searchInput.focus()
+            },0)
+          }
+        }
       },
     {
       title: 'Operasi',
       dataIndex: 'operation',
-      scopedSlots: {customRender: 'operation'}
+      scopedSlots: {
+          customRender: 'operation'
+        }
     }
 
       ];
@@ -80,6 +164,15 @@ export default {
     this.getCases();
   },
   methods: {
+    handleSearch (selectedKeys, confirm) {
+      confirm()
+      this.searchText = selectedKeys[0]
+    },
+
+    handleReset (clearFilters) {
+      clearFilters()
+      this.searchText = ''
+    },
     getCases() {
       let self = this;
       axios
